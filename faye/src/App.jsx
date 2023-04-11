d2ef339bd3883bc3b74de1e39de4327d19effa9f
Otis Wales <therunningbeta@gmail.com>
2023-04-11 13:50:19 +1200

    ADD changed position

--- faye/src/App.jsx
+++ faye/src/App.jsx
@@ -15,17 +15,17 @@ function App() {
           <div className="h-16 w-full bg-slate-500 flex justify-center items-center y2k-shadow4"></div>
           <div className="flex bg-slate-500 h-[52rem] m-4  ">
             {/* Left Side */}
-            <div className="w-1/2  bg-slate-500 flex flex-col ">
-              <div className="h-5/6 bg-blue-300 mx-4 mt-2 rounded-xl y2k-shadow2 flex justify-center items-center  ">
+            <div className="w-2/3  bg-slate-500 flex flex-col ">
+              <div className="h-5/6 bg-blue-300 mx-1 mt-2 rounded-xl y2k-shadow2 flex justify-center items-center  ">
                 <div className="h-full rounded-2xl w-full bg-cover y2k-shadow4    bg-[url('./assets/faye.png')]"></div>
               </div>
-              <div className="h-1/6 bg-gradient-to-t from-[#8ea7aa] to-[#53686b]  mx-4 mt-4 mb-2 rounded-xl y2k-shadow2 "></div>
+              <div className="h-1/6 bg-gradient-to-t from-[#8ea7aa] to-[#53686b]   mb-2 mt-4  rounded-2xl y2k-shadow4 mx-4 p-16  "></div>
             </div>
             {/* Right Side */}
             <div className="w-1/2   flex flex-col">
-              <div className="h-1/6 bg-gradient-to-t from-[#cee3e5] to-[#b4cdcf] m-2 rounded-md y2k-shadow4 "></div>
-              <div className="h-2/6 bg-gradient-to-t from-[#cee3e5] to-[#b4cdcf] m-2 rounded-md y2k-shadow4"></div>
-              <div className="h-3/6  flex m-2">
+              <div className="h-1/6 bg-gradient-to-t from-[#cee3e5] to-[#b4cdcf] mx-2 mt-2 rounded-md y2k-shadow4 "></div>
+              <div className="h-2/6 bg-gradient-to-t from-[#cee3e5] to-[#b4cdcf] mx-2 mt-2 rounded-md y2k-shadow4"></div>
+              <div className="h-3/6  flex mx-2 mt-2">
                 <div className="bg-slate-500 h-full w-full flex flex-col ">
                   <div className="h-full bg-gradient-to-t from-[#cee3e5] to-[#b4cdcf] mb-2 rounded-md y2k-shadow4"></div>
                   <div className="h-4/6 bg-gradient-to-t from-[#8ea7aa] to-[#53686b] y2k-shadow3 rounded-md mt-2 mb-2"></div>
