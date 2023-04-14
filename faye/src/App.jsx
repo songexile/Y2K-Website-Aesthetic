f951452ba0a957d3ced42256844c42f09419b4b8
Otis Wales <therunningbeta@gmail.com>
2023-04-14 17:20:12 +1200

    ADD: new styling

--- faye/src/App.jsx
+++ faye/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 <div className="App">
   <div className="mx-auto w-2/3 h-screen flex items-center justify-center y2k-shadow3 relative border-y2k">
     {/* Pseudo-element for background image */}
-    <div className="absolute inset-0 bg-[url('./assets/faey2.png')] bg-opacity-20 z-0"></div>
+    <div className="absolute inset-0  bg-opacity-20 z-0 shadow-xl "></div>
     {/* Pseudo-element for background color */}
     <div className="absolute inset-0 bg-slate-900 bg-opacity-50 z-0"></div>
     {/* Main Div/Container */}
@@ -23,7 +23,56 @@ function App() {
           <div className="h-5/6 bg-blue-300 mx-1 mt-2 rounded-xl y2k-shadow2 flex justify-center items-center ">
             <div className="h-full rounded-2xl  w-full bg-cover y2k-shadow4 bg-[url('./assets/faye.png')] inner-stroke " ></div>
           </div>
-          <div className="h-1/6 bg-gradient-to-t from-[#8ea7aa] to-[#53686b] mb-1 mt-4 rounded-2xl y2k-shadow4 mx-1 p-16 inner-stroke"></div>
+          <div className="h-1/6 bg-gradient-to-t from-[#8ea7aa] to-[#53686b] mb-1 mt-4 rounded-2xl y2k-shadow4 mx-1 p-4 inner-stroke">
+    <div className="flex items-center justify-center h-full">
+        <div className="w-1/2 flex flex-col items-start justify-center pr-2">
+            <div className="w-full bg-gray-400 m-1 y2k-shadow3 flex">
+                <div className="text-right p-1 text-white bg-green-500 w-1/3 y2k-shadow3"></div>
+            </div>
+            <div className="w-full bg-gray-400 m-1 y2k-shadow3 flex">
+                <div className="text-right p-1 text-white bg-green-500 w-1/2 y2k-shadow3"></div>
+            </div>
+            <div className="w-full bg-gray-400 m-1 y2k-shadow3 flex">
+                <div className="text-right p-1 text-white bg-green-500 w-1/2 y2k-shadow3"></div>
+            </div>
+            <div className="w-full bg-gray-400 m-1 y2k-shadow3 flex">
+                <div className="text-right p-1 text-white bg-green-500 w-1/3 y2k-shadow3"></div>
+            </div>
+            <div className="w-full bg-gray-400 m-1 y2k-shadow3 flex">
+                <div className="text-right p-1 text-white bg-green-500 w-1/2 y2k-shadow3"></div>
+            </div>
+            <div className="w-full bg-gray-400 m-1 y2k-shadow3 flex">
+                <div className="text-right p-1 text-white bg-green-500 w-1/2 y2k-shadow3"></div>
+            </div>
+        </div>
+        <div className="w-1/2 flex flex-col items-start justify-center pl-2">
+            <div className="w-full  m-1  flex">
+                <div className="text-right p-1 text-white bg-green-500 w-1/3 y2k-shadow3"></div>
+            </div>
+            <div className="w-full  m-1  flex">
+                <div className="text-right p-1 text-white bg-green-500 w-1/2 y2k-shadow3"></div>
+            </div>
+            <div className="w-full  m-1  flex">
+                <div className="text-right p-1 text-white bg-green-500 w-1/2 y2k-shadow3"></div>
+            </div>
+            <div className="w-full  m-1  flex">
+                <div className="text-right p-1 text-white bg-green-500 w-1/3 y2k-shadow3"></div>
+            </div>
+            <div className="w-full  m-1  flex">
+                <div className="text-right p-1 text-white bg-green-500 w-1/2 y2k-shadow3"></div>
+            </div>
+            <div className="w-full  m-1  flex">
+                <div className="text-right p-1 text-white bg-green-500 w-1/2 y2k-shadow3"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
         </div>
         {/* Right Side */}
         <div className="w-1/2 flex flex-col ">
