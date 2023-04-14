af8a573b04a7aed8b51bd650a7e1d36d0b453f17
Otis Wales <therunningbeta@gmail.com>
2023-04-14 21:12:29 +1200

    added buttons

--- faye/tailwind.config.js
+++ faye/tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      "pixelated-sans": ["Pixelated MS Sans Serif", "sans-serif"],
+    },
+    extend: {
+      fontFamily: {
+        default: ["Pixelated MS Sans Serif", "sans-serif", "!important"],
+      },
+      button: {}, // disable all default button styles
+    },
   },
   plugins: [],
 };
