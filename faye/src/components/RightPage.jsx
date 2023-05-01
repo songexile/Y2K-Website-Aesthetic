80b3f58de32d6d26d18b5c1fc921fb6029db34d5
songexile <therunningbeta@gmail.com>
2023-05-01 19:02:43 +1200

    netlify push

--- faye/src/components/RightPage.jsx
+++ faye/src/components/RightPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function RightPage({
   currentModelIndex,
@@ -6,6 +6,19 @@ function RightPage({
   totalModels,
   carDescription,
 }) {
+  const [displayedText, setDisplayedText] = useState("");
+
+  useEffect(() => {
+    let index = 0;
+    const timer = setInterval(() => {
+      setDisplayedText(carDescription.substring(0, index));
+      index++;
+      if (index > carDescription.length) {
+        clearInterval(timer);
+      }
+    }, 50);
+    return () => clearInterval(timer);
+  }, [carDescription]);
   return (
     <div className="w-1/2 flex flex-col ">
       <div className="h-1/6 bg-gradient-to-t from-[#cee3e5] to-[#b4cdcf] mx-2 mt-2 rounded-md y2k-shadow4 inner-stroke flex justify-evenly flex-col sm:flex-row items-center">
@@ -49,7 +62,7 @@ function RightPage({
             <div class="title-bar-text">About</div>
             <div class="title-bar-controls"></div>
           </div>
-          <div className="flex mx-4 mt-2 justify-center items-center  text-center ">
+          <div className="flex mx-4 mt-2 justify-center items-center  text-center  ">
             {carDescription}
           </div>
         </div>
@@ -59,7 +72,7 @@ function RightPage({
           <div className="h-full bg-gradient-to-t from-[#cee3e5] to-[#b4cdcf] mb- rounded-md y2k-shadow4 inner-stroke"></div>
           <div className="h-4/6 bg-gradient-to-t from-[#8ea7aa] to-[#53686b] y2k-shadow3 rounded-md mt-2 mb-1 inner-stroke flex items-center justify-center">
             <a href="https://sketchfab.com/wersaus33">
-              <div className=" bg-white y2k-shadow4 ">
+              <div className=" bg-[#8ba9b0]  y2k-shadow4 ">
                 <button className="p-4">
                   Made with the help of models from Wersaus33
                 </button>
